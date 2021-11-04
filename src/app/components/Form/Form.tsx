d8ae95fa997089d2447aa18ac9d0688af384bcea
Manuel Schmid <89474622+manusmd@users.noreply.github.com>
2021-11-04 10:07:24 +0100

    :sparkles: Add POST request on submit

--- src/app/components/Form/Form.tsx
+++ src/app/components/Form/Form.tsx
@@ -1,14 +1,26 @@
-import React, { useState } from 'react';
+import React, { FormEvent, useState } from 'react';
 import styles from './Form.module.css';
 
 function Form(): JSX.Element {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  console.log(firstName);
-  console.log(lastName);
+
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    fetch('https://json-server.machens.dev/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName: firstName,
+        lastName: lastName,
+      }),
+    });
+  }
 
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <input
         type="text"
         className={styles.formText}
